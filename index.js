853ca1c00511e05f4af1be4280b58b188915ce71
zhaofuyun <zhaofuyun202@gmail.com>
2018-01-17 15:55:28 +0800

    refactor: Replace PhantomJS with Puppeteer

--- index.js
+++ index.js
@@ -1,41 +1,38 @@
-var Nightmare = require('nightmare');
 var fs = require('fs-extra');
 var argv = require('yargs').argv;
 var path = require('path');
+var config = require('config');
+var puppeteer = require('puppeteer');
+var to = require('await-to-js').default;
 
 if(!argv.url) {
     console.error('缺少参数--url');
     process.exit(1);
 }
 
-var defaultPath = 'dist/' + (new Date).getTime() + '.pdf';
-var output = path.resolve(argv.path || defaultPath);
-fs.mkdirsSync(path.dirname(output));
-
-var nightmare = Nightmare({
-    waitTimeout: argv.timeout // ms
-});
-    
-nightmare
-    .viewport(1300, 1000)
-    .goto(argv.url)
-    .then(function (data) {
-        if(data.code !== 200) {
-            throw new Error('Failed: HTTP status=' + data.code);
-        }
-
-        return nightmare
-            .wait(argv.selector || 0)
-            .pdf(output, {
-                pageSize: 'A4',
-                printBackground: true
-            })
-            .end();
-    })
-    .then(function () {
-        console.log('pdf已生成:', output);
-    })
-    .catch(function (err) {
-        console.error(err.message);
-        process.exit(1);
+(async () => {
+    var browser = await puppeteer.launch({
+        // headless: false,
+        executablePath: config.executablePath
     });
+    var page = await browser.newPage();
+    await page.goto(argv.url);
+    if(argv.selector) {
+        var promise = page.waitForSelector(argv.selector, {timeout: argv.timeout});
+        var [err, result] = await to(promise);
+    }
+    if(err) {
+        console.error(err.message);
+        return await browser.close();
+    }
+    var defaultPath = 'dist/' + Date.now() + '.pdf';
+    var output = path.resolve(argv.path || defaultPath);
+    fs.ensureDir(path.dirname(output));
+    await page.pdf({
+        path: output,
+        format: 'A4',
+        printBackground: true
+    });
+    await browser.close();
+    console.log(`pdf已生成：${output}`);
+})();
--- readme.md
+++ readme.md
@@ -2,35 +2,23 @@
 
 根据html页面生成pdf
 
-![supported](https://img.shields.io/badge/Win_7-ok-brightgreen.svg)
-![supported](https://img.shields.io/badge/Mac_OS_X-ok-brightgreen.svg)
-![supported](https://img.shields.io/badge/Ubuntu-ok-brightgreen.svg)
-![unsupported](https://img.shields.io/badge/CentOS-fail-red.svg)
-
 ## 安装
 
-    如果使用Ubuntu系统，请先安装依赖
-    
-    sudo apt-get install -y Xvfb libgtk2.0-0 libxtst6 libxss1 libgconf-2-4 libasound2
-    
-    如果终端提示缺少某个依赖，请在 http://packages.ubuntu.com/ 里查找到相应`软件包内容`并安装
+1. 自行安装[Chromium](https://download-chromium.appspot.com/)，或者[Chrome Canary](https://www.google.com/intl/en/chrome/browser/canary.html)
+2. 运行 `PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true npm install`
+3. 新建文件`config/local.json`，填写Chromium的绝对路径，例如
 
-运行`npm install`
+```js
+{
+    "executablePath": "/Applications/Chromium.app/Contents/MacOS/Chromium"
+}
+```
 
+## 如何使用
 
-## 使用
-
-- Mac, Win 7
-
-    ```
-    node index.js <options>
-    ```
-
-- Ubuntu
-
-    ```
-    xvfb-run -a node index.js <options>
-    ```
+```
+node index.js <options>
+```
 
 ### options
 
@@ -43,12 +31,6 @@ timeout | 超时时间（单位`ms`），默认30s
 
 ## 示例
 
-- `node index.js --url https://www.baidu.com/`
-- 调试模式下运行：
-    
-    ```
-    cross-env DEBUG=nightmare:*,electron:* node index.js --url https://www.baidu.com/
-    ```
-## 常见问题
-
-- ubuntu下生成的pdf乱码：安装字体 `sudo apt-get install -y fonts-noto-cjk`
+```
+node index.js --url http://news.qq.com
+```
